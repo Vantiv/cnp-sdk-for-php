bd08d7d9cf6a856501fc222eee87b5d172a93869
dlabovit <devenlabovitch57@gmail.com>
2019-06-10 14:15:32 -0400

    Change test field

--- cnp/sdk/Test/functional/CnpResponseProcessorFunctionalTest.php
+++ cnp/sdk/Test/functional/CnpResponseProcessorFunctionalTest.php
@@ -14,6 +14,11 @@ class CnpResponseProcessorFunctionalTest extends \PHPUnit_Framework_TestCase
     private $config;
     private $sale;
 
+
+    public function test_setgdfg(){
+        $this->assertEquals("Valid Format", "Valid Format");
+    }
+    
 //    public static function setUpBeforeClass()
 //    {
 //        CommManager::reset();
