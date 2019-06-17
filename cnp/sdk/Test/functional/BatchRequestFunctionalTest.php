7acaee3a287237d71bac34284b3f7102b5874a72
dlabovit <devenlabovitch57@gmail.com>
2019-06-17 15:01:08 -0400

    comment unused stream test

--- cnp/sdk/Test/functional/BatchRequestFunctionalTest.php
+++ cnp/sdk/Test/functional/BatchRequestFunctionalTest.php
@@ -1505,48 +1505,48 @@ class BatchRequestFunctionalTest extends \PHPUnit_Framework_TestCase
 //        $this->assertEquals(0, $response);
 //    }
 
-    public function test_sendToCnpStream()
-    {
-        $sale_info = array(
-            'id' => '1',
-            'orderId' => '1',
-            'amount' => '10010',
-            'orderSource'=>'ecommerce',
-            'billToAddress'=>array(
-                'name' => 'John Smith',
-                'addressLine1' => '1 Main St.',
-                'city' => 'Burlington',
-                'state' => 'MA',
-                'zip' => '01803-3747',
-                'country' => 'US'),
-            'card'=>array(
-                'number' =>'5112010000000003',
-                'expDate' => '0112',
-                'cardValidationNum' => '349',
-                'type' => 'MC')
-        );
-
-        $cnp_request = new CnpRequest();
-        $batch_request = new BatchRequest();
-
-        # add a sale to the batch
-        $batch_request->addSale($sale_info);
-        # close the batch, indicating that we intend to add no more sales
-        $batch_request->closeRequest();
-        # add the batch to the cnp request
-        $cnp_request->addBatchRequest($batch_request);
-        # close the cnp request, indicating that we intend to add no more batches
-        $cnp_request->closeRequest();
-        # send the batch to cnp via SFTP
-        $response_file = $cnp_request->sendToCnpStream();
-        # process the response file
-        $resp = new CnpResponseProcessor($response_file);
-
-        $message = $resp->getXmlReader()->getAttribute("message");
-        $response = $resp->getXmlReader()->getAttribute("response");
-        $this->assertEquals("Valid Format", $message);
-        $this->assertEquals(0, $response);
-    }
+//    public function test_sendToCnpStream()
+//    {
+//        $sale_info = array(
+//            'id' => '1',
+//            'orderId' => '1',
+//            'amount' => '10010',
+//            'orderSource'=>'ecommerce',
+//            'billToAddress'=>array(
+//                'name' => 'John Smith',
+//                'addressLine1' => '1 Main St.',
+//                'city' => 'Burlington',
+//                'state' => 'MA',
+//                'zip' => '01803-3747',
+//                'country' => 'US'),
+//            'card'=>array(
+//                'number' =>'5112010000000003',
+//                'expDate' => '0112',
+//                'cardValidationNum' => '349',
+//                'type' => 'MC')
+//        );
+//
+//        $cnp_request = new CnpRequest();
+//        $batch_request = new BatchRequest();
+//
+//        # add a sale to the batch
+//        $batch_request->addSale($sale_info);
+//        # close the batch, indicating that we intend to add no more sales
+//        $batch_request->closeRequest();
+//        # add the batch to the cnp request
+//        $cnp_request->addBatchRequest($batch_request);
+//        # close the cnp request, indicating that we intend to add no more batches
+//        $cnp_request->closeRequest();
+//        # send the batch to cnp via SFTP
+//        $response_file = $cnp_request->sendToCnpStream();
+//        # process the response file
+//        $resp = new CnpResponseProcessor($response_file);
+//
+//        $message = $resp->getXmlReader()->getAttribute("message");
+//        $response = $resp->getXmlReader()->getAttribute("response");
+//        $this->assertEquals("Valid Format", $message);
+//        $this->assertEquals(0, $response);
+//    }
 
     public function tearDown()
     {
