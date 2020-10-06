fde1cf7b16384b66096bcbc3780cf71669882603
kmgreg <kmgregorek@gmail.com>
2020-10-06 13:19:35 -0400

    Use correct xml parse for test

--- cnp/sdk/Test/certification/CertAlphaTest.php
+++ cnp/sdk/Test/certification/CertAlphaTest.php
@@ -156,6 +156,8 @@ class CertAlphaTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('11111', trim(XmlParser::getNode($saleResponse, 'authCode')));
         $this->assertEquals('1', XmlParser::getNode($saleResponse, 'avsResult'));
         $this->assertEquals('M', XmlParser::getNode($saleResponse, 'cardValidationResult'));
+        $pinlessResponse = XmlParser::getNodeWithChildren($saleResponse, 'pinlessDebitResponse');
+        $this->assertEquals('VI', $pinlessResponse->networkName);
 
         $credit_hash = array(
             'cnpTxnId' => (XmlParser::getNode($saleResponse, 'cnpTxnId')),
