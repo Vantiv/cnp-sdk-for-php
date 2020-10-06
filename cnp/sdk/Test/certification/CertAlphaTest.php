542401dad6fcc005bc0e7144e888b2805dfe14db
kmgreg <kmgregorek@gmail.com>
2020-10-06 11:17:44 -0400

    Added test pinless response

--- cnp/sdk/Test/certification/CertAlphaTest.php
+++ cnp/sdk/Test/certification/CertAlphaTest.php
@@ -156,6 +156,8 @@ class CertAlphaTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('11111', trim(XmlParser::getNode($saleResponse, 'authCode')));
         $this->assertEquals('1', XmlParser::getNode($saleResponse, 'avsResult'));
         $this->assertEquals('M', XmlParser::getNode($saleResponse, 'cardValidationResult'));
+        $pinlessResponse = XmlParser::getNode($saleResponse, 'pinlessDebitResponse');
+        $this->assertEquals('VI', XmlParser::getNode($pinlessResponse, 'networkName'));
 
         $credit_hash = array(
             'cnpTxnId' => (XmlParser::getNode($saleResponse, 'cnpTxnId')),
