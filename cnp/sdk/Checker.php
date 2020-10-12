6ed7d1f9678e2912342d9759d2d8b296cf05b4e2
GregoryConrad <gsconrad@wpi.edu>
2020-10-12 15:41:05 -0400

    Added basic changes for upgrade

--- cnp/sdk/Checker.php
+++ cnp/sdk/Checker.php
@@ -36,7 +36,7 @@ class Checker
     public static function validateXML($request){
         $xml = new DOMDocument();
         $xml->loadXML($request);
-        $filepath = __DIR__ . "/schema/SchemaCombined_v12.15.xsd";
+        $filepath = __DIR__ . "/schema/SchemaCombined_v12.16.xsd";
         $result =  $xml->schemaValidate( $filepath);
 
         if(!$result)
--- cnp/sdk/CnpOnline.php
+++ cnp/sdk/CnpOnline.php
@@ -24,8 +24,8 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 namespace cnp\sdk;
-define('CURRENT_XML_VERSION', '12.15');
-define('CURRENT_SDK_VERSION', 'PHP;12.15.0');
+define('CURRENT_XML_VERSION', '12.16');
+define('CURRENT_SDK_VERSION', 'PHP;12.16.0');
 define('MAX_TXNS_PER_BATCH', 100000);
 define('MAX_TXNS_PER_REQUEST', 500000);
 define('CNP_CONFIG_LIST', 'user,password,merchantId,timeout,proxy,reportGroup,version,url,cnp_requests_path,batch_requests_path,sftp_username,sftp_password,batch_url,tcp_port,tcp_ssl,tcp_timeout,print_xml,vantivPublicKeyID,gpgPassphrase,useEncryption,deleteBatchFiles,multiSite,multiSiteErrorThreshold,maxHoursWithoutSwitch,printMultiSiteDebug,multiSiteUrl1,multiSiteUrl2,sftp_timeout');
