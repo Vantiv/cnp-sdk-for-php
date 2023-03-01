38032864cb91d4c6cc2b19c4de14062021cb6613
sghadage <sushamaghadage38@gmail.com>
2023-03-01 08:49:21 -0500

    Adding xsd changes till v12.29.

--- cnp/sdk/Checker.php
+++ cnp/sdk/Checker.php
@@ -36,7 +36,7 @@ class Checker
     public static function validateXML($request){
         $xml = new DOMDocument();
         $xml->loadXML($request);
-        $filepath = __DIR__ . "/schema/SchemaCombined_v12.27.xsd";
+        $filepath = __DIR__ . "/schema/SchemaCombined_v12.29.xsd";
         $result =  $xml->schemaValidate( $filepath);
 
         if(!$result)
